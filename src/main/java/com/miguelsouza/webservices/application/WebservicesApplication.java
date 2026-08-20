d529062562b0161fc197e310654bfb08cc2c2879
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-20 18:16:10 -0300

    User entity and resource

--- src/main/java/com/miguelsouza/webservices/application/WebservicesApplication.java
+++ src/main/java/com/miguelsouza/webservices/application/WebservicesApplication.java
@@ -1,4 +1,4 @@
-package com.miguelsouza.webservices;
+package com.miguelsouza.webservices.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
