46ba8ac07e8d5b97a59311345709d66cd755611a
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-21 18:14:13 -0300

    User update

--- src/main/java/com/miguelsouza/webservices/services/UserService.java
+++ src/main/java/com/miguelsouza/webservices/services/UserService.java
@@ -3,7 +3,11 @@ package com.miguelsouza.webservices.services;
 import com.miguelsouza.webservices.entities.User;
 import com.miguelsouza.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,4 +34,17 @@ public class UserService {
     public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 }
