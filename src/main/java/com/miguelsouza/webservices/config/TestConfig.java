20a2d77d0affb738d79a6c76e9f4f354facdab81
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-21 13:51:59 -0300

    Category

--- src/main/java/com/miguelsouza/webservices/config/TestConfig.java
+++ src/main/java/com/miguelsouza/webservices/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.miguelsouza.webservices.config;
 
+import com.miguelsouza.webservices.entities.Category;
 import com.miguelsouza.webservices.entities.Order;
 import com.miguelsouza.webservices.entities.User;
 import com.miguelsouza.webservices.entities.enums.OrderStatus;
+import com.miguelsouza.webservices.repositories.CategoryRepository;
 import com.miguelsouza.webservices.repositories.OrderRepository;
 import com.miguelsouza.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @Configuration
@@ -23,9 +26,19 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
--- src/main/java/com/miguelsouza/webservices/services/OrderService.java
+++ src/main/java/com/miguelsouza/webservices/services/OrderService.java
@@ -1,9 +1,7 @@
 package com.miguelsouza.webservices.services;
 
 import com.miguelsouza.webservices.entities.Order;
-import com.miguelsouza.webservices.entities.User;
 import com.miguelsouza.webservices.repositories.OrderRepository;
-import com.miguelsouza.webservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
