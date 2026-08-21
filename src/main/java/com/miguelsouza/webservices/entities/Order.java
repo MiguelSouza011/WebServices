29ce1beeae0e5d6664fe021c83fe83b90d694ac9
Miguel Souza <souzzamiguel09@gmail.com>
2026-08-21 13:24:35 -0300

    OrderStatus enum

--- src/main/java/com/miguelsouza/webservices/entities/Order.java
+++ src/main/java/com/miguelsouza/webservices/entities/Order.java
@@ -1,6 +1,7 @@
 package com.miguelsouza.webservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.miguelsouza.webservices.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,15 +19,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -36,6 +41,16 @@ public class Order implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
